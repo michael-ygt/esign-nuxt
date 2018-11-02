e0d7d56697212b8bd9ee97c865bd921b0d14c90e
michael-ygt <michael.cavanagh@yourgolftravel.com>
2018-11-02 16:18:36 +0000

    Add purgecss to reduce bundle sizes

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,26 @@
 const join = require("path").join;
-const tailwindJS = join(__dirname, "tailwind.js");
+const tailwindConfig = join(__dirname, "tailwind.js");
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || []; // eslint-disable-line no-useless-escape
+  }
+}
 
 module.exports = {
-  plugins: [require("tailwindcss")(tailwindJS), require("autoprefixer")]
+  plugins: [
+    require("tailwindcss")(tailwindConfig),
+    require("autoprefixer"),
+    require("@fullhuman/postcss-purgecss")({
+      content: ["**/*.vue"],
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          extensions: ["vue"]
+        }
+      ],
+      whitelist: ["html", "body"],
+      whitelistPatterns: [/nuxt-/]
+    })
+  ]
 };
